809b4d39f4481e423383165f92702828a05ef67c
Teague <teague.omara@selu.edu>
2022-06-13 08:30:25 -0500

    BCI

--- script/BCITree.R
+++ script/BCITree.R
@@ -9,6 +9,12 @@ fams <- batDat %>% dplyr::select(fam, Species_Name)
 bciCons <- consensus(bciTree, p=1, check.labels = TRUE, rooted = TRUE)
 names <- as.data.frame(bciCons$tip.label)
 names <- names %>% left_join(fams, by = c("bciCons$tip.label" = "Species_Name"))
+library(stringr)
+
+bciCons$tip.label <- str_replace(bciCons$tip.label, "Mimon_crenulatum","Gardnernycteri_crenulatum") 
+bciCons$tip.label <- str_replace(bciCons$tip.label, "Molossus_currentium","Molossus_bondae")
+bciCons$tip.label <- str_replace(bciCons$tip.label, "_"," ")
+
 
 bciCons$family <- names$fam
 p <- ggtree(bciCons)+
@@ -33,31 +39,31 @@ colnames(edge)=c("parent", "node", "edge_num")
 p <- ggtree(bciCons)+
   geom_tiplab(align=TRUE,size=2)+
   xlim(NA, 30)
-p+geom_cladelab(node=19, 
+p+geom_cladelab(node=20, 
                  label="Natalidae",
                  offset=5.5, align=TRUE, size = 2)+
-   geom_cladelab(node=79, 
+   geom_cladelab(node=82, 
                label="Phyllostomidae",
                color="red2", offset=5.5, align=TRUE)+
-  geom_cladelab(node=116,
+  geom_cladelab(node=121,
                 label="Mormoopidae",
                 offset=5.5, align=TRUE, size = 2)+
-  geom_cladelab(node=120,
+  geom_cladelab(node=125,
                 label="Noctilionidae",
                 offset=5.5, align=TRUE, size = 2)+
-  geom_cladelab(node=119,
+  geom_cladelab(node=124,
                 label="Thyropteridae",
                 offset=5.5, align=TRUE, size = 2)+
-  geom_cladelab(node=123,
+  geom_cladelab(node=128,
                 label="Molossidae",
                 offset=5.5, align=TRUE, size = 2)+
-  geom_cladelab(node=127,
+  geom_cladelab(node=132,
                 label="Vespertilionidae",
                 offset=5.5, align=TRUE, size = 2)+
-  geom_cladelab(node=134, 
+  geom_cladelab(node=139, 
                 label="Emballonuridae",
                 offset=5.5, align=TRUE, size = 2)
-ggsave("./BCI_Phylogeny.pdf", width = 8, height = 10)  
+ggsave("./BCI_PhylogenyTO_Vertlife Upham2019.pdf", width = 8, height = 10)  
   
   
 
